81df9dcf5f063d7e12f720ed40447620788b442e
Anthony Russell Jr <92353075+russgithubs@users.noreply.github.com>
2023-10-03 06:52:36 +0000

    wordy-masters.js was updated

--- wordy-masters.js
+++ wordy-masters.js
@@ -145,6 +145,29 @@ async function init() {
   });
 }
 
+  // Add touch event listeners
+  document.addEventListener("touchstart", function handleTouchStart(event) {
+    // Handle touch events here
+    // You can determine which element was touched and take action accordingly
+    // For example, if a virtual keyboard button is touched, you can simulate key presses
+    // Here's a general example:
+    
+    // Get the touched element
+    const touchedElement = event.target;
+
+    // Check if the touched element represents a virtual keyboard button (customize this part)
+    if (touchedElement.classList.contains("virtual-keyboard-button")) {
+      // Get the key value from the touched element (customize this part)
+      const key = touchedElement.innerText;
+      
+      // Simulate a key press by calling your addLetter function with the key
+      addLetter(key.toUpperCase()); // Assuming you have an addLetter function
+    }
+  });
+
+// ... (rest of your code)
+
+
 // a little function to check to see if a character is alphabet letter
 // this uses regex (the /[a-zA-Z]/ part) but don't worry about it
 // you can learn that later and don't need it too frequently
